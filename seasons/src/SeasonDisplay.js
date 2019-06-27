aa52af8b04f19b0ec52d6e5525ff6f2eb76458d5
zakevicius <m.zakevicius@gmail.com>
2019-06-27 22:53:04 +0300

    seasons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,35 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        icon: 'sun'
+    },
+    winter: {
+        text: 'Brr, it\'s chilly',
+        icon: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, icon } = seasonConfig[season];
+
     return (
-        <div>Season Display</div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive icon ${icon}`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive icon ${icon}`} />
+        </div>
     );
 };
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
-const App = () => {
-    return (
-        <div>Heloo</div>
-    );
+class App extends React.Component {
+
+// State initialization in constructor
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMesage: '' };        
+    // }
+
+// State initialization without constructor
+    state = { lat: null, errorMesage: '' };
+
+// Autmoatic function when component is rendered
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            error => this.setState({ errorMesage: error.message })
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMesage && !this.state.lat) {
+            return <div>Error: {this.state.errorMesage}</div>;
+        }
+        if (!this.state.errorMesage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        return <Loader message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div style={{border: '5px solid red'}}>
+                {this.renderContent()}
+            </div>
+        )
+    }
 };
 
 ReactDOM.render(
