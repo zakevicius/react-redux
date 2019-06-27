af24f131247b771a0e0da048e92a7e035db79c23
zakevicius <m.zakevicius@gmail.com>
2019-06-27 20:21:59 +0300

    seasons

---
+++ seasons/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>Heloo</div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
