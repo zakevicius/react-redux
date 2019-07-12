fbbb4e6d9ddf153f1ced73b325c0d153143e2770
zakevicius <m.zakevicius@gmail.com>
2019-07-12 20:55:21 +0300

    Remade with hooks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,46 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Loader from './Loader';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-
-// State initialization in constructor
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { lat: null, errorMesage: '' };        
-    // }
-
-// State initialization without constructor
-    state = { lat: null, errorMesage: '' };
-
-// Autmoatic function when component is rendered
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude}),
-            error => this.setState({ errorMesage: error.message })
-        );
+const App = () => {
+    const [lat, errorMessage] = useLocation();
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />
+    } else {
+        content = <Loader message="Please accept location request" />
     }
 
-    renderContent() {
-        if (this.state.errorMesage && !this.state.lat) {
-            return <div>Error: {this.state.errorMesage}</div>;
-        }
-        if (!this.state.errorMesage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-        return <Loader message="Please accept location request" />;
-    }
-
-    render() {
-        return (
-            <div style={{border: '5px solid red'}}>
-                {this.renderContent()}
-            </div>
-        )
-    }
+    return <div className="border red">{content}</div>
 };
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
-)
+);
